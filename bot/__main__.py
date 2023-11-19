28a5f814050396778d8c0ff10d8519c6e560a1c3
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-11-19 07:36:35 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -116,8 +116,7 @@ async def _skiped_ul(event):
     REQUEST.append(True)
     await asyncio.gather(
         *[
-            geter("https://subsplease.org/rss/?r=720", index),
-            geter("https://subsplease.org/rss/?r=1080p", index),
+            geter("https://subsplease.org/rss/?r=480", index),  
         ]
     )
     REQUEST.clear()
@@ -192,7 +191,7 @@ async def upload(torrent_link, name, compress=False):
                     f"```New File Downloaded, Named {name}\nNow Going To Commpress```",
                     buttons=[[Button.inline("STATS", data=f"tas_{_code}")]],
                 )
-                cmd = f'''{Var.FFMPEG} -i """{dl}""" -metadata "Encoded By"="t.me/Anime_Flares" -preset ultrafast -c:v libx265 -crf 25 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? """{out}""" -y'''
+                cmd = f'''{Var.FFMPEG} -i """{dl}""" -metadata "Encoded By"="ANIME ZENITH" -preset veryfast -c:v libx265 -crf 28 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? """{out}""" -y'''
                 process = await asyncio.create_subprocess_shell(
                     cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
                 )
@@ -322,7 +321,7 @@ bot.loop.run_until_complete(notify_about_me())
 # Webhook for upload and other stuffs
 try:
     bot.loop.run_until_complete(
-        syst("https://subsplease.org/rss/?r=720", "https://subsplease.org/rss/?r=1080")
+        syst("https://subsplease.org/rss/?r=480")
     )
 
     # loop
